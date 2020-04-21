d9bbb4d679f13ed7cab366fbdd174fb937a26ec2
guilhermeaw <guilherme.w@universo.univates.br>
2020-04-20 23:43:18 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, obj) => {
+        return acc + obj.value;
+      }, 0);
+
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, obj) => {
+        return acc + obj.value;
+      }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,49 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Cannot outcome transaction without a valid balance');
+    }
+
+    const createCategory = new CreateCategoryService();
+
+    const createdCategory = await createCategory.execute({
+      title: category,
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: createdCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
